42f489b7d6b7d4d0be3c06edfbb60b469b941dff
Gonzalo Sosa <gonzasosa@outlook.com>
2023-03-20 17:52:14 -0600

    No iterable version

--- Collections/IteratorBasics/src/main/java/mx/uv/fiee/iinf/poo/collections/Main.java
+++ Collections/IteratorBasics/src/main/java/mx/uv/fiee/iinf/poo/collections/Main.java
@@ -1,23 +1,22 @@
 package mx.uv.fiee.iinf.poo.collections;
 
+import java.util.NoSuchElementException;
+
 public class Main {
 
     public static void main(String [] args) {
         var filmVector = new Film [10];
 
         var myCol = new MyMarvelCollection (filmVector);
-	myCol.add (new Film ("Ironman", 2008));
-	myCol.add (new Film ("Captain America", 2010));
-	myCol.add (new Film ("The Avengers", 2012));
+        myCol.add (new Film ("Ironman", 2008));
+        myCol.add (new Film ("Captain America", 2010));
+        myCol.add (new Film ("The Avengers", 2012));
 
-        var iterator = myCol.iterator ();
-        while (iterator.hasNext ()) {
-            System.out.println (iterator.next ());
-        }
-
-        for (var film: myCol) {
-            System.out.println (film);
-        }
+        try {
+            for (int i = 0; i < myCol.size (); i++) {
+                System.out.println (myCol.get (i).toString ());
+            }
+        } catch (ArrayIndexOutOfBoundsException | NoSuchElementException ex) {}
     }
 
 }
